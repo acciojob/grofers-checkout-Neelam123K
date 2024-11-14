fe23f2f583bff4263919be541ba51b37ba454025
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2024-11-14 17:22:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ document.body.appendChild(getSumBtn);
 const getSum = () => {
 //Add your code here
 	const prices = document.querySelectorAll('.prices');
-    let total = 0;
+    let getsum = 0;
 
-    prices.forEach(price => {
-        total += parseFloat(price.textContent);
+    prices.forEach(prices => {
+        total += parseFloat(getsum.textContent);
     });
 
     let existingTotalRow = document.querySelector("#totalRow");
     if (existingTotalRow) {
-        existingTotalRow.first.textContent = `Total Price: $${total}`;
+        existingTotalRow.first.textContent = `Total Prices: ${getsum}`;
     } else {
         const totalRow = document.createElement('tr');
-        totalRow.id = "totalRow"; // Set an id to identify the total row
+        totalRow.id = "totalRow"; 
         const totalCell = document.createElement('td');
         totalCell.colSpan = 2;
-        totalCell.textContent = `Total Price: $${total}`;
+        totalCell.textContent = `Total Prices: $${total}`;
         totalRow.appendChild(totalCell);
 
         document.getElementById('groceryTable').appendChild(totalRow);
